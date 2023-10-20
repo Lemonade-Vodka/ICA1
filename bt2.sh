7d70d5731b786d73a20e168f3c0dce17c6947a69
Lemonade-Gin <yxu128903@gmail.com>
2023-10-20 12:48:43 +0100

    a little update of bt2 shell script

--- bt2.sh
+++ bt2.sh
@@ -1,3 +1,5 @@
+bowtie2-build /localdisk/data/BPSM/ICA1/Tcongo_genome/* index
+
 for pair in `ls ~/ICA1/fastq_cut2 |cut -d '_' -f1,2 | uniq`
 do
  bowtie2 -x ~/ICA1/index/index -1 ~/ICA1/fastq_cut2/$pair'_1.fq' -2 ~/ICA1/fastq_cut2/$pair'_2.fq' -S ~/ICA1/bt2_output/$pair'.sam'
